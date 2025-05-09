fd2fa37431ae4918815c33e7a9247208d39b59e6
willzer0 <pembunuh4444@gmail.com>
2025-05-09 09:12:04 +0800

    100% fix topnav

--- src/app/component/navbar/index.tsx
+++ src/app/component/navbar/index.tsx
@@ -2,13 +2,18 @@
 'use client';
 
 import './index.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { motion } from 'framer-motion';
 
 export default function Navbar() {
   return (
+    <motion.div
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -50 }}
+      transition={{ duration: 0.4 }}>
     <nav className="navbar navbar-expand-lg navbar-dark mt-4">
       <div className="container-fluid">
-        <a id="logo" className="navbar-brand" href="#"><img src="/willzer0.png" alt="Logo" height="40" /></a>
+        <a id="logo" className="navbar-brand" href="#"><img src="/willzer0.png" alt="Logo" height="50" /></a>
         <button
           className="navbar-toggler"
           type="button"
@@ -20,21 +25,22 @@ export default function Navbar() {
         <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a id="home-nav" className="nav-link active text-black fw-semibold fs-6" href="/">Home</a>
+              <a id="home-nav" className="nav-link active fw-semibold fs-6" href="/">Home</a>
             </li>
             <li className="nav-item">
-              <a id="home-nav" className="nav-link active text-black fw-semibold fs-6" href="/">About</a>
+              <a id="home-nav" className="nav-link active fw-semibold fs-6" href="/">About</a>
             </li>
             <li className="nav-item">
-              <a id="home-nav" className="nav-link active text-black fw-semibold fs-6" href="/">Skill</a>
+              <a id="home-nav" className="nav-link active fw-semibold fs-6" href="/">Skill</a>
             </li>
             <li className="nav-item">
-              <a id="home-nav" className="nav-link active text-black fw-semibold fs-6" href="/">Project</a>
+              <a id="home-nav" className="nav-link active fw-semibold fs-6" href="/">Project</a>
             </li>
           </ul>
         </div>
-        
+        <a href="https://github.com/willzer0" className="git" ><img src="https://th.bing.com/th?q=GitHub+PNG+White&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-ID&cc=ID&setlang=en&adlt=strict&t=1&mw=247" alt="Github" height="50" className='gitimage' /></a>
       </div>
     </nav>
+    </motion.div>
   );
 }
